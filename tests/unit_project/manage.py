fe48a6f14a9dc757369edb59f4aa134a7f4efa29
Jan Seifert <jan.seifert@centrumholdings.com>
2011-06-30 10:59:36 +0200

    fixed manage.py in tests/unit_project, fixed django 1.3 compatibility, refs #12839

--- tests/unit_project/manage.py
+++ tests/unit_project/manage.py
@@ -11,7 +11,7 @@ from django.core.management import execute_from_command_line
 sys.path.insert(0, abspath(join(dirname(__file__), pardir, pardir)))
 sys.path.insert(0, abspath(join(dirname(__file__), pardir)))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'unit_project.settings'
 
 if __name__ == "__main__":
     execute_from_command_line()
--- tests/unit_project/settings.py
+++ tests/unit_project/settings.py
@@ -1,10 +1,20 @@
 from os import path
 
+from django import VERSION
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = ':memory:'
+if VERSION[:2] >= (1, 3):
+    DATABASES = {
+            'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        },
+    }
+else:
+    DATABASE_ENGINE = 'sqlite3'
+    DATABASE_NAME = ':memory:'
 
 ROOT_URLCONF = 'test_project.urls'
 
